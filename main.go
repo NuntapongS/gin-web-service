a7685b84cfdfb36957b77a5aef9b40fed1687a0f
Nuntapong <nuntapong@odds.team>
2022-07-26 14:17:02 +0700

    [pat] update name for safe

--- main.go
+++ main.go
@@ -14,8 +14,8 @@ type entity struct {
 }
 
 var entities = []entity {
-	{Id: "1", Name: "Nuntapong Siripanyawong", Gender: "Male", Email: "-"},
-	{Id: "2", Name: "Kanyanat Nomsian", Gender: "Female", Email: "-"},
+	{Id: "1", Name: "John snow", Gender: "Male", Email: "-"},
+	{Id: "2", Name: "Red Magenta", Gender: "Female", Email: "-"},
 
 }
 
